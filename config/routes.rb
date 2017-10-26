1e2c2ed222d8cfa632a5c27091aff54c1324dea8
revjatto <revjatto@yahoo.co.uk>
2017-10-26 16:06:34 +0000

    Added new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/blog'
+
   resources :keylogs
-  get 'keylogs/blog'
+ 
   root to: 'keylogs#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
